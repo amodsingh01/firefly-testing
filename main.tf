1deff14cfe6ab7e026b1a08ad781dff6747e1e28
amodsingh01 <54638981+amodsingh01@users.noreply.github.com>
2023-10-04 17:31:41 +0530

    Composed by AI: main.tf

--- main.tf
+++ main.tf
@@ -8,3 +8,16 @@ module "s3_bucket" {
   bucket_name = var.bucket_name
   # tags = var.tags
 }
+
+provider "aws" {
+  region = var.region
+}
+
+module "s3_bucket" {
+  source = "./modules/s3_bucket"
+
+  bucket_name = var.bucket_name
+  tags = {
+    Environment = "dev"
+  }
+}
